fb460b8684e25ca396d360a3d6f3a66bfb24ac28
Paul Lietar <paul@lietar.net>
2013-11-27 03:29:06 +0000

    Add a postgresql addon.

---
+++ postgresql/start-pgsql.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+PREFIX=/usr/lib/postgresql/9.1/bin/
+DATADIR=/opt/postgresql/data
+EXTRA_ARGS="-D $DATADIR -h '*' -c config_file=/etc/postgresql/9.1/main/postgresql.conf"
+
+
+if [[ ! -d $DATADIR ]]; then
+  PASSWORD=$(</opt/postgresql/PASSWORD)
+
+  mkdir -p $DATADIR
+  chown postgres:postgres $DATADIR
+
+  su postgres -c "$PREFIX/initdb -D $DATADIR"
+  su postgres -c "$PREFIX/postgres --single $EXTRA_ARGS" <<< "ALTER USER postgres WITH PASSWORD '$PASSWORD';"
+fi
+
+su postgres -c "$PREFIX/postgres $EXTRA_ARGS"
+
